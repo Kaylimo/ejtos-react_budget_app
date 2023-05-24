861bbb98d50cfc944d9b15ee159a0ff504b9214d
Kaylimo <jp.turowicz@gmail.com>
2023-05-24 06:31:36 -0400

    all tasks

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,7 +1,20 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
 const Currency = () => {
+
+    const { dispatch } = useContext(AppContext);
+
+    const submitEvent = (value) => {
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: value
+        });
+    }
+
     return (
         <div className={'alert'}>
-            <select id="currencySelect" style={{ backgroundColor: 'palegreen' }}>
+            <select id="currencySelect" style={{ backgroundColor: 'palegreen' }} onChange={(event) => submitEvent(event.target.value)}>
                 <option value="$" name="dollar">$ Dollar</option>
                 <option value="£" name="pound">£ Pound</option>
                 <option value="€" name="euro">€ Euro</option>
